d96724d1df0a7f4f31636c70c513e0d163784677
Alfath <alfathhr1101@gmail.com>
2026-08-13 07:29:20 +0700

    fix api base url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,12 +1,10 @@
-const API_BASE = 'http://127.0.0.1:3001/api';
+const API_BASE = 'https://103.193.179.47/api';
 
 // =========================
 // LOGS
 // =========================
 export async function getLogs() {
-
   const res = await fetch(`${API_BASE}/logs`);
-
   return res.json();
 }
 
@@ -14,9 +12,7 @@ export async function getLogs() {
 // ADMIN ONLINE
 // =========================
 export async function getAdmins() {
-
   const res = await fetch(`${API_BASE}/admin-status`);
-
   return res.json();
 }
 
@@ -24,50 +20,35 @@ export async function getAdmins() {
 // BANK MASTER
 // =========================
 export async function getBanks() {
-
   const res = await fetch(`${API_BASE}/banks`);
-
   return res.json();
 }
 
 export async function createBank(data) {
-
   const res = await fetch(`${API_BASE}/banks`, {
-
     method: 'POST',
-
     headers: {
       'Content-Type': 'application/json'
     },
-
     body: JSON.stringify(data)
   });
-
   return res.json();
 }
 
 export async function updateBank(id, data) {
-
   const res = await fetch(`${API_BASE}/banks/${id}`, {
-
     method: 'PUT',
-
     headers: {
       'Content-Type': 'application/json'
     },
-
     body: JSON.stringify(data)
   });
-
   return res.json();
 }
 
 export async function deleteBank(id) {
-
   const res = await fetch(`${API_BASE}/banks/${id}`, {
-
     method: 'DELETE'
   });
-
   return res.json();
 }
